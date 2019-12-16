ad4519c581d3a1a2b454f5621184b39edb32cc58
diegosalas27 <dominicsc2hs@gmail.com>
2019-12-16 16:31:54 -0500

    changes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "../components/common/Header";
 import Banner from "../components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,12 +12,10 @@ function App() {
   return (
     <Router>
       <Header />
-      <div className="main">
-        <Route exact path="/" component={Banner} />
-        <Route exact path="/AboutMe" component={About} />
-        <Route exact path="/Jobs" component={Jobs} />
-        <Route exact path="/Projects" component={Projects} />
-      </div>
+      <Route exact path="/" component={Banner} />
+      <Route exact path="/AboutMe" component={About} />
+      <Route exact path="/Jobs" component={Jobs} />
+      <Route exact path="/Projects" component={Projects} />
 
       <Footer />
     </Router>
